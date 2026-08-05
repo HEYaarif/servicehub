ea30881a5efcdedc4d3eefc8145fa62477983650
Md Aarif <aarif.mdaarif2002@gmail.com>
2026-08-05 17:33:10 +0530

    fix: correct Navbar import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
-import Navbar from "./Components/layout/Navbar";
+import Navbar from "./Components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
